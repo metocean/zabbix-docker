39727103f2e347ef34ebf31910b3d280a6b63709
Andrew Parnasov <a.parnasov@metocean.co.nz>
2020-02-19 16:10:48 +1300

    Added support for telegram sender to docker image

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ else
     VCS_REF=$MAJOR_VERSION.$MINOR_VERSION
 fi
 
-docker build -t zabbix-$app_component:$os-$version --build-arg VCS_REF="$VCS_REF" --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -f Dockerfile .
+docker build -t metocean/zabbix-$app_component:$os-$version --build-arg VCS_REF="$VCS_REF" --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -f Dockerfile .
 
 if [ "$type" != "build" ]; then
     links=""
